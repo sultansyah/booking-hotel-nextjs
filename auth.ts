a8ea6a16d2b50d287e9d89e3de5b45ba88113379
sultansyah <muhammadsultansyah14@gmail.com>
2025-07-07 15:09:59 +0700

    feat: add model table

--- auth.ts
+++ auth.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import {prisma} from "@/lib/prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+    adapter: PrismaAdapter(prisma),
     providers: [Google],
     session: { strategy: "jwt" },
     pages: {
