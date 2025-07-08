dc5c0ded26bf56ac800d005a9df36e694ababb56
sultansyah <muhammadsultansyah14@gmail.com>
2025-07-08 10:00:52 +0700

    feat: add middleware

---
+++ middleware.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@/auth";
+
+const ProtectedRoutes = [
+    "/myreservation",
+    "/checkout",
+    "/admin"
+]
+
+export async function middleware(request: NextRequest) {
+    const session = await auth()
+    const isLoggedIn = !!session?.user
+    const role = session?.user.role
+    const { pathname } = request.nextUrl
+
+    if (!isLoggedIn && ProtectedRoutes.some(route => pathname.startsWith(route))) {
+        return NextResponse.redirect(new URL("/signin", request.url))
+    }
+
+    if (isLoggedIn && role !== "admin" && pathname.startsWith("/admin")) {
+        return NextResponse.redirect(new URL("/", request.url))
+    }
+
+    if (isLoggedIn && pathname.startsWith("/signin")) {
+        return NextResponse.redirect(new URL("/", request.url))
+    }
+}
+
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    ],
+}
